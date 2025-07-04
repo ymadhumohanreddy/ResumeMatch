254fdee18336cc178735e3446042c6610f18487b
MARTHALA REDDY (RA2211030010226) <mr1809@srmist.edu.in>
2025-07-04 18:28:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -11,6 +11,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, React.ComponentProps<'tex
           className
         )}
         ref={ref}
+        suppressHydrationWarning
         {...props}
       />
     );
